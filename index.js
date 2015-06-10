70e618201d0b2d11448e23c2110f8ee7681ff4a8
Albert Callarisa <shark234@gmail.com>
2015-06-10 12:48:22 -0300

    Added some documentation

--- index.js
+++ index.js
@@ -7,21 +7,27 @@ var baseConfig = {
   version: 1
 }
 
+// Gets a configuration value from either the `configuration` or the default
 function getConfig(configuration, key){
   if(configuration && configuration.hasOwnProperty(key))
     return configuration[key]
   return baseConfig[key]
 };
 
+// Returns the full url for a given endpoint.
 function endpoint(client, endpointPath) {
   return url.resolve(client.url, path.join("/v"+client.version, endpointPath));
 }
 
+// Constructor for the routific client object
 function Routific(configuration) {
   this.url = getConfig(configuration, 'url');
   this.version = getConfig(configuration, 'version');
 };
 
+// Performs a call to the login endpoint.
+// The callback will be called with `error` and `body`, being `body` the
+//   parsed body of the response.
 Routific.prototype.login = function(email, password, cb) {
   request({
     url: endpoint(this, "/users/login"),
